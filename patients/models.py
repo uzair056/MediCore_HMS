f8af4b86bf1fb7f0605d46e3ba2104ecb017d6fb
Muhammad Uzair <uzair.khan.122399@gmail.com@example.com>
2026-07-20 18:55:43 +0500

    Add password field to Patient model and create migration

--- patients/models.py
+++ patients/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Patient(models.Model):
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
+    password = models.CharField(max_length=255)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
