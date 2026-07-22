3ea9b1e56fcba45d0debe47f0918d7e3ba1af1bb
Muhammad Uzair <uzair.khan.122399@gmail.com@example.com>
2026-07-22 17:57:46 +0500

    Implement weather API integration and update home page to display dynamic weather information

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.hashers import check_password
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password
-
+import requests
 
 from users.models import User
 
@@ -15,8 +15,16 @@ def login(request):
 def admin_dashboard(request):
     return render(request, "users/admin_dashboard.html")
 
+
 def home(request):
-    return render(request, "home/home.html")
+
+    weather = weather_api("Islamabad")
+
+    context = {
+        "weather": weather
+    }
+
+    return render(request, "home/home.html", context)
 
 def store(request):
     if request.method == "POST":
@@ -51,3 +59,51 @@ def user_login(request):
 
     return redirect("login")
 
+def weather_api(city="Islamabad"):
+    try:
+        # Geocoding API
+        geo_url = (
+            f"https://geocoding-api.open-meteo.com/v1/search"
+            f"?name={city}&count=1&language=en&format=json"
+        )
+
+        geo_data = requests.get(geo_url).json()
+
+        if "results" not in geo_data:
+            return None
+
+        location = geo_data["results"][0]
+
+        latitude = location["latitude"]
+        longitude = location["longitude"]
+
+        # Weather API
+        weather_url = (
+            "https://api.open-meteo.com/v1/forecast"
+            f"?latitude={latitude}"
+            f"&longitude={longitude}"
+            "&current="
+            "temperature_2m,"
+            "relative_humidity_2m,"
+            "apparent_temperature,"
+            "weather_code,"
+            "wind_speed_10m,"
+            "visibility"
+        )
+
+        current = requests.get(weather_url).json()["current"]
+
+        return {
+            "city": location["name"],
+            "country": location["country"],
+            "temperature": round(current["temperature_2m"]),
+            "humidity": current["relative_humidity_2m"],
+            "feels_like": round(current["apparent_temperature"]),
+            "wind": round(current["wind_speed_10m"]),
+            "visibility": round(current["visibility"] / 1000),
+            "weather_code": current["weather_code"],
+        }
+
+    except Exception as e:
+        print("Weather API Error:", e)
+        return None
